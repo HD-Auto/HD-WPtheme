6126b57eaa661730f05ec76a2836aeab47df11b2
Brodie Owens <brodie@onyxclient.com>
2025-07-11 15:49:54 +1000

    add contact and copyright

--- woonuxt_base/app/pages/contact.vue
+++ woonuxt_base/app/pages/contact.vue
@@ -2,19 +2,26 @@
   <div class="container my-8">
     <h1 class="mb-8 text-3xl font-semibold text-primary">Contact Page</h1>
     <div class="my-8 prose">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat facilis, commodi illum repellat accusamus praesentium quaerat ut ducimus ipsam deserunt, a voluptate id
-        similique ullam, provident tenetur molestiae ad. Ea aperiam esse eaque aut, beatae consectetur nam velit unde! Et excepturi quam eos labore, nulla impedit recusandae
-        quaerat maxime expedita.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo voluptate neque in odio officia assumenda nulla cumque. Veniam et tenetur accusantium, fuga pariatur
-        repellendus impedit. Quod sint odio quisquam! Sequi repellendus eum dolorum eveniet, quidem dolor necessitatibus velit sunt esse maiores. Eum odit dolorum mollitia?
-        Adipisci amet hic veniam debitis architecto nobis necessitatibus inventore iure ullam cum illo quae quam, excepturi beatae itaque exercitationem magnam quidem aspernatur
-        iusto. Quos, reprehenderit? Dignissimos autem recusandae qui sit cum velit consequatur pariatur illum omnis excepturi, adipisci ab eius, optio, temporibus iure unde
-        inventore. Maxime officiis tempore voluptatum ipsa, ab possimus atque corporis repellendus obcaecati, numquam fuga ullam doloremque ea reprehenderit earum quam culpa neque
-        dolor sint voluptates qui rem? Ipsam reiciendis non illum?
-      </p>
+      <h3>
+        Call us!
+      </h3>
+
+      <p>07 2103 1141</p>
+
+      <p><strong>Email at:</strong> sales@hdautomotive.com.au</p>
+
+      <h4>Visit us instore at our Rocklea location:</h4>
+
+      <p>8/29 Collinsvale St, Rocklea QLD 4106</p>
+
+      <p><strong>Operating Hours:</strong></p>
+      <ul>
+        <li>Monday        – 8am to 5pm</li>
+        <li>Tuesday       – 8am to 5pm</li>
+        <li>Wednesday     – 8am to 5pm</li>
+        <li>Thursday      – 8am to 5pm</li>
+        <li>Friday        – 8am to 5pm</li>
+      </ul>
     </div>
   </div>
 </template>
